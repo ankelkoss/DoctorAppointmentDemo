16d8719db20be1db6be07899686c47cb43be5390
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-12 00:05:25 +0200

    Update Program.cs

---
+++ DoctorAppointmentDemo.UI/DoctorAppointment.cs
@@ -0,0 +1,52 @@
+﻿using DoctorAppointmentDemo.Domain.Entities;
+using DoctorAppointmentDemo.Service.Interfaces;
+using DoctorAppointmentDemo.Service.Services;
+
+namespace DoctorAppointmentDemo.UI
+{
+    public class DoctorAppointment
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorAppointment()
+        {
+            _doctorService = new DoctorService();
+        }
+
+        public void Menu()
+        {
+            //while (true)
+            //{
+            //    // add Enum for menu items and describe menu
+            //}
+
+            Console.WriteLine("Current doctors list: ");
+            var docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+            }
+
+            Console.WriteLine("Adding doctor: ");
+
+            var newDoctor = new Doctor
+            {
+                Name = "Vasya",
+                Surname = "Petrov",
+                Experience = 20,
+                DoctorType = Domain.Enums.DoctorTypes.Dentist
+            };
+
+            _doctorService.Create(newDoctor);
+
+            Console.WriteLine("Current doctors list: ");
+            docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+            }
+        }
+    }
+}
