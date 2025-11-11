df902f465a1043272da4c1dff24b4d2c0bae0e36
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-11 22:38:01 +0200

    Create AppointmentRepository.cs

---
+++ DoctorAppointmentDemo.Data/Repositories/AppointmentRepository.cs
@@ -0,0 +1,41 @@
+﻿using DoctorAppointmentDemo.Data.Interfaces;
+using DoctorAppointmentDemo.Domain.DbConfig;
+using MyDoctorAppointment.Data.Configuration;
+using MyDoctorAppointment.Data.Repositories;
+using MyDoctorAppointment.Domain.Entities;
+using Newtonsoft.Json;
+
+namespace DoctorAppointmentDemo.Data.Repositories
+{
+    public class AppointmentRepository : GenericRepository<Appointment>, IAppointmentRepository
+    {
+        public override string Path { get; set; }
+
+        public override int LastId { get; set; }
+
+        public AppointmentRepository()
+        {
+            JsonConfig result = base.ReadFromAppSettings().Database.Appointments;
+
+            Path = result.Path;
+            LastId = result.LastId;
+        }
+
+        public override void ShowInfo(Appointment source)
+        {
+            //TODO: implement this
+            Console.WriteLine();
+        }
+
+        protected override void SaveLastId()
+        {
+            AppDbConfig result = base.ReadFromAppSettings();
+
+            result = base.MoveGlobalPathAppSettings(result);
+
+            result.Database.Doctors.LastId = LastId;
+
+            File.WriteAllText(Constants.AppSettingsPath, JsonConvert.SerializeObject(result, Formatting.Indented));
+        }
+    }
+}
