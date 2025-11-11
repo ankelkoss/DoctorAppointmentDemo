32e7887aa27e3e06b11149a978144c27dcb54c4e
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-11 20:45:58 +0200

    Create IPatientService.cs

---
+++ DoctorAppointmentDemo.Service/Interfaces/IPatientService.cs
@@ -0,0 +1,17 @@
+﻿using MyDoctorAppointment.Domain.Entities;
+
+namespace DoctorAppointmentDemo.Service.Interfaces
+{
+    public interface IPatientService
+    {
+        Patient Create(Patient patient);
+
+        IEnumerable<Patient> GetAll();
+
+        Patient? Get(int id);
+
+        bool Delete(int id);
+
+        Patient Update(int id, Patient patient);
+    }
+}
