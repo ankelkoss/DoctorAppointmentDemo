e21070a16985dc3deee0d44e12943c8666bd0b9b
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-11 18:02:20 +0200

    Create DatabaseConfig.cs

---
+++ DoctorAppointmentDemo.Domain/DbConfig/DatabaseConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace DoctorAppointmentDemo.Domain.DbConfig
+{
+    public sealed class DatabaseConfig
+    {
+        public JsonConfig Doctors { get; set; } = new();
+        public JsonConfig Patients { get; set; } = new();
+        public JsonConfig Appointments { get; set; } = new();
+    }
+}
