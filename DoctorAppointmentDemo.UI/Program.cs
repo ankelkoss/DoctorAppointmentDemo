320837d927b260676f2164fbd0e88bf8cf2132ff
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-12 00:49:16 +0200

    Update Program.cs

--- DoctorAppointmentDemo.UI/Program.cs
+++ DoctorAppointmentDemo.UI/Program.cs
@@ -1,11 +1,46 @@
-﻿namespace DoctorAppointmentDemo.UI
+﻿using DoctorAppointmentDemo.Domain.Entities;
+using DoctorAppointmentDemo.Service.Services;
+
+namespace DoctorAppointmentDemo.UI
 {
     public static class Program
     {
         public static void Main()
         {
-            var doctorAppointment = new DoctorAppointment();
-            doctorAppointment.Menu();
+            //var doctorAppointment = new DoctorAppointment();
+            //doctorAppointment.Menu();
+
+            var patientService = new PatientService();
+            var doctorService = new DoctorService();
+            var appointmentService = new AppointmentService();
+
+            Doctor doctor = doctorService.GetAll().First();
+
+            Patient patinet = new Patient()
+            {
+                Name = "Тарас",
+                Surname = "Тарасович",
+                Address = "Украина, Киев, ул. Прорезная 12 кв 3",
+                Phone = "+380931234567",
+                Email = "taras.tarasovich@gmail.com",
+                AdditionalInfo = "somithing",
+                IllnessType = Domain.Enums.IllnessTypes.EyeDisease
+            };
+
+            patinet = patientService.Create(patinet);
+
+            var appointment = new Appointment()
+            {
+                Doctor = doctor,
+                Patient = patinet,
+                DateTimeFrom = DateTime.Parse("20.11.2025 14:00"),
+                DateTimeTo = DateTime.Parse("20.11.2025 15:00"),
+                Description = "Something"
+            };
+
+            var r = appointmentService.Create(appointment);
+
+
         }
     }
 }
