83f1a297b007a6890b9850834af4381088c3be78
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-11 22:37:58 +0200

    Create IAppointmentRepository.cs

---
+++ DoctorAppointmentDemo.Data/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using DoctorAppointmentDemo.Data.Interfaces.Base;
+using MyDoctorAppointment.Domain.Entities;
+
+namespace DoctorAppointmentDemo.Data.Interfaces
+{
+    public interface IAppointmentRepository : IGenericRepository<Appointment>
+    {
+    }
+}
