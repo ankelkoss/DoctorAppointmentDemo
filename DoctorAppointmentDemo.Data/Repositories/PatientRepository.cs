61ddaefd4dfdea64d04e65c701d6a57353698574
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-11 20:37:11 +0200

    Create PatientRepository.cs

---
+++ DoctorAppointmentDemo.Data/Repositories/PatientRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using DoctorAppointmentDemo.Data.Interfaces;
+using DoctorAppointmentDemo.Domain.DbConfig;
+using MyDoctorAppointment.Data.Configuration;
+using MyDoctorAppointment.Data.Interfaces;
+using MyDoctorAppointment.Data.Repositories;
+using MyDoctorAppointment.Domain.Entities;
+using Newtonsoft.Json;
+
+namespace DoctorAppointmentDemo.Data.Repositories
+{
+    public class PatientRepository : GenericRepository<Patient>, IPatientRepository
+    {
+        public override string Path { get; set; }
+
+        public override int LastId { get; set; }
+
+        public PatientRepository()
+        {
+            AppDbConfig result = base.ReadFromAppSettings();
+
+            Path = result.Database.Patients.Path;
+            LastId = result.Database.Patients.LastId;
+        }
+
+        public override void ShowInfo(Patient source)
+        {
+            //TODO: implement this
+            throw new NotImplementedException();
+        }
+
+        protected override void SaveLastId()
+        {
+            AppDbConfig result = base.ReadFromAppSettings();
+
+            result = base.MoveGlobalPathAppSettings(result);
+
+            result.Database.Patients.LastId = LastId;
+
+            File.WriteAllText(Constants.AppSettingsPath, JsonConvert.SerializeObject(result, Formatting.Indented));
+        }
+    }
+}
