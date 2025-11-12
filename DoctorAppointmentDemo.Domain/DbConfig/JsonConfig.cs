c4657aab3ca166f4ee188016bcc63e160c2f1fcf
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-12 17:40:09 +0200

    Update JsonConfig.cs

--- DoctorAppointmentDemo.Domain/DbConfig/JsonConfig.cs
+++ DoctorAppointmentDemo.Domain/DbConfig/JsonConfig.cs
@@ -2,7 +2,22 @@
 {
     public sealed class JsonConfig
     {
-        public int LastId { get; set; } = 0;
-        public string Path { get; set; } = "";
+        public int LastId { get; set; }
+        public string Path { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is JsonConfig config)
+            {
+                return Path == config.Path;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Path.GetHashCode();
+        }
     }
 }
