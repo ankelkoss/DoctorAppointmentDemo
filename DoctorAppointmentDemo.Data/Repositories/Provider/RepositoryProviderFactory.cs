055bd26e6376e9ea5cfdb5f9c83921a26a022ec7
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-26 19:43:21 +0200

    Create RepositoryProviderFactory.cs

---
+++ DoctorAppointmentDemo.Data/Repositories/Provider/RepositoryProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿using DoctorAppointmentDemo.Data.Interfaces.Base;
+using DoctorAppointmentDemo.Domain.Enums;
+
+namespace DoctorAppointmentDemo.Data.Repositories.Provider
+{
+    public static class RepositoryProviderFactory
+    {
+        public static IRepositoryProvider Create(StorageTypeEnum storageType)
+        {
+            return storageType switch
+            {
+                StorageTypeEnum.Json => new JsonRepositoryProvider(),
+                StorageTypeEnum.Xml => new XmlRepositoryProvider(),
+      
+                _ => throw new ArgumentOutOfRangeException(nameof(storageType), storageType, null)
+            };
+        }
+    }
+}
